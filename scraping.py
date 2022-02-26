689f6c7ea1cc642ee0a9b71470002eeb730595aa
PDob02 <patrickdobry@gmail.com>
2022-02-26 09:29:15 -0600

    Adding module example of scraping.py

--- scraping.py
+++ scraping.py
@@ -1,29 +1,29 @@
-# Import Splinter and BeautifulSoup
+# Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Set up splinter:
+# Set up Splinter
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
-# Visit the mars nasa news site
-url = 'https://redplanetscience.com'
+# Visit the Mars news site
+url = 'https://redplanetscience.com/'
 browser.visit(url)
 
 # Optional delay for loading the page
 browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-# convert html to a soup object
+# Convert the browser html to a soup object and then quit the browser
 html = browser.html
 news_soup = soup(html, 'html.parser')
 
 slide_elem = news_soup.select_one('div.list_text')
+
 slide_elem.find('div', class_='content_title')
 
-
-# Use the parent element to find the first `a` tag and save it as `news_title`
+# Use the parent element to find the first a tag and save it as `news_title`
 news_title = slide_elem.find('div', class_='content_title').get_text()
 news_title
 
@@ -65,6 +65,3 @@ df
 df.to_html()
 
 browser.quit()
-
-
-
